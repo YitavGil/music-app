a30d73cd3644acc3818c4e46a6738b5981da0cd1
YitavGil <yitavga@yahoo.com>
2022-10-07 10:48:11 +0300

    song cards

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,9 +1,14 @@
+import { useDispatch, useSelector } from 'react-redux';
+
 import { Error, Loader, SongCard } from '../components';
 import { genres } from '../assets/constants';
 
 import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 
 const Discover = () => {
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+
   const { data, isFetching, error } = useGetTopChartsQuery();
   const genreTitle = 'Rock';
 
@@ -32,7 +37,14 @@ const Discover = () => {
 
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
         {data?.map((song, index) => (
-          <SongCard key={song.key} song={song} index={index} />
+          <SongCard
+            key={song.key}
+            song={song}
+            index={index}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            data={data}
+          />
         ))}
       </div>
     </div>
